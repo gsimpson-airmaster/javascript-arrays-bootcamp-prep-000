a59fb3434a1825790b2ae4dc4ea3964928f3e3c5
gsimpson@airmaster.com.au <gsimpson@airmaster.com.au>
2018-06-30 10:40:41 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -36,4 +36,9 @@ function removeElementFromBeginningOfArray(array) {
   return array.slice(1);
 }
 
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop();
+  return array;
+}
+
 function 
